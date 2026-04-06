e50df64957df14cce78ba66652f5bd94ed925d31
hakchangs <hakchangs@gmail.com>
2026-04-06 16:02:19 +0900

    fix: add loglevel to serverExternalPackages to share same instance with instrumentation

--- service/fe-web-nextjs/next.config.ts
+++ service/fe-web-nextjs/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  serverExternalPackages: ['@opentelemetry/api-logs'],
+  serverExternalPackages: ['@opentelemetry/api-logs', 'loglevel'],
 };
 
 export default nextConfig;
