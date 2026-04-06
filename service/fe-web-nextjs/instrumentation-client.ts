2f49b273c224d80ae23b6ae1562d72e1a93c33a3
hakchangs <hakchangs@gmail.com>
2026-04-06 18:21:42 +0900

    test: remove FetchInstrumentation to isolate browser span export issue

--- service/fe-web-nextjs/instrumentation-client.ts
+++ service/fe-web-nextjs/instrumentation-client.ts
@@ -4,7 +4,6 @@ import { WebTracerProvider, BatchSpanProcessor } from '@opentelemetry/sdk-trace-
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { Resource } from '@opentelemetry/resources';
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
-import { FetchInstrumentation } from '@opentelemetry/instrumentation-fetch';
 import { DocumentLoadInstrumentation } from '@opentelemetry/instrumentation-document-load';
 
 const exporter = new OTLPTraceExporter({
@@ -29,9 +28,5 @@ provider.register();
 registerInstrumentations({
   instrumentations: [
     new DocumentLoadInstrumentation(),
-    new FetchInstrumentation({
-      // OTLP export 요청 자체를 추적에서 제외 (순환 간섭 방지)
-      ignoreUrls: [/\/api\/otlp\//],
-    }),
   ],
 });
