367ecb4b265fd086a01ac8c7d9823237997d7282
hakchangs <hakchangs@gmail.com>
2026-04-16 17:24:43 +0900

    client-side guid 생성 테스트

--- service/be-bff/src/main/java/net/kubeworks/bebff/filter/GuidFilter.java
+++ service/be-bff/src/main/java/net/kubeworks/bebff/filter/GuidFilter.java
@@ -8,6 +8,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,6 +18,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Component
+@Order(1)
 public class GuidFilter extends OncePerRequestFilter {
 
     private static final DateTimeFormatter FORMATTER =
