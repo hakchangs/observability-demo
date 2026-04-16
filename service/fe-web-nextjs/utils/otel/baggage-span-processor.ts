f513b857cc3e4896b5d2e620555291e732d79773
hakchangs <hakchangs@gmail.com>
2026-04-16 18:33:18 +0900

    client-side guid 레이스 컨디션 문제 해소 테스트

--- service/fe-web-nextjs/utils/otel/baggage-span-processor.ts
+++ service/fe-web-nextjs/utils/otel/baggage-span-processor.ts
@@ -1,11 +1,12 @@
 import {ReadableSpan, Span, SpanProcessor} from "@opentelemetry/sdk-trace-web";
 import {Context, propagation} from "@opentelemetry/api";
 
+// baggage 에 담긴 모든 값을 span 에 저장
 export class BaggageToAttributesProcessor implements SpanProcessor {
     onStart(span: Span, parentContext: Context): void {
         const baggage = propagation.getBaggage(parentContext);
         baggage?.getAllEntries().forEach(([key, entry]) => {
-            span.setAttribute(`baggage.${key}`, entry.value);
+            span.setAttribute(`${key}`, entry.value);
         });
     }
     onEnd(_span: ReadableSpan): void {}
