499caf11ee609b99ceab37bb3b45eecc23e2db09
hakchangs <hakchangs@gmail.com>
2026-05-26 15:27:59 +0900

    fe-web-nextjs: propagation 코드 정리 > 정상처리 - 코드 정리

--- service/fe-web-nextjs/instrumentation.ts
+++ service/fe-web-nextjs/instrumentation.ts
@@ -8,28 +8,5 @@ export async function register() {
     const { setupLogBridge } = await import('./utils/otel/logger.server');
     setupLogBridge();
 
-    // override fetch() > baggage header 추가
-    //   const originalFetch = globalThis.fetch;
-    //   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    //   (globalThis as any).fetch = async function patchedFetch(
-    //       input: RequestInfo | URL,
-    //       init?: RequestInit,
-    //   ) {
-    //       try {
-    //           const { headers } = await import('next/headers');
-    //           const reqHeaders = await headers();
-    //           const baggage = reqHeaders.get('baggage');
-    //           if (baggage) {
-    //               const outHeaders = new Headers(init?.headers);
-    //               if (!outHeaders.has('baggage')) {
-    //                   outHeaders.set('baggage', baggage);
-    //               }
-    //               return originalFetch(input, { ...init, headers: outHeaders });
-    //           }
-    //       } catch {
-    //           // request context 외부(헬스체크, 초기화 등)에서는 그냥 통과
-    //       }
-    //       return originalFetch(input, init);
-    //   };
   }
 }
