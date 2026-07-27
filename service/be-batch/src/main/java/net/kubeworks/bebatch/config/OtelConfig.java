3e4c19dce7388c58b6f841ed95151f7a4fd3733b
hakchangs <hakchangs@gmail.com>
2026-07-27 15:02:27 +0900

    Otel bridge 제외 테스트: jdbc 수집 확인

--- service/be-batch/src/main/java/net/kubeworks/bebatch/config/OtelConfig.java
+++ service/be-batch/src/main/java/net/kubeworks/bebatch/config/OtelConfig.java
@@ -1,14 +1,14 @@
 package net.kubeworks.bebatch.config;
 
-import io.micrometer.observation.Observation;
-import io.micrometer.observation.ObservationHandler;
-import io.micrometer.observation.ObservationRegistry;
-import io.micrometer.tracing.Tracer;
-import io.micrometer.tracing.handler.DefaultTracingObservationHandler;
-import io.micrometer.tracing.otel.bridge.OtelCurrentTraceContext;
-import io.micrometer.tracing.otel.bridge.OtelTracer;
-import io.opentelemetry.api.GlobalOpenTelemetry;
-import io.opentelemetry.api.OpenTelemetry;
+//import io.micrometer.observation.Observation;
+//import io.micrometer.observation.ObservationHandler;
+//import io.micrometer.observation.ObservationRegistry;
+//import io.micrometer.tracing.Tracer;
+//import io.micrometer.tracing.handler.DefaultTracingObservationHandler;
+//import io.micrometer.tracing.otel.bridge.OtelCurrentTraceContext;
+//import io.micrometer.tracing.otel.bridge.OtelTracer;
+//import io.opentelemetry.api.GlobalOpenTelemetry;
+//import io.opentelemetry.api.OpenTelemetry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
@@ -19,30 +19,30 @@ public class OtelConfig {
 
     private final Logger log = LoggerFactory.getLogger(OtelConfig.class);
 
-    @Bean
-    OpenTelemetry openTelemetry() {
-        return GlobalOpenTelemetry.get();   // agent SDK 재사용 — 이게 핵심
-    }
-
-    @Bean
-    Tracer tracer() {
-        var otel = GlobalOpenTelemetry.get();   // agent SDK
-        var currentTraceContext = new OtelCurrentTraceContext();
-        return new OtelTracer(
-                otel.getTracer("spring-batch"),
-                currentTraceContext,
-                event -> {}             // EventPublisher no-op
-        );
-    }
-
-    @Bean
-    ObservationRegistry batchObservationRegistry(Tracer tracer) {
-        var registry = ObservationRegistry.create();
-        DefaultTracingObservationHandler observationHandler = new DefaultTracingObservationHandler(tracer);
-        registry.observationConfig()
-                .observationHandler(observationHandler);
-        return registry;
-    }
+//    @Bean
+//    OpenTelemetry openTelemetry() {
+//        return GlobalOpenTelemetry.get();   // agent SDK 재사용 — 이게 핵심
+//    }
+//
+//    @Bean
+//    Tracer tracer() {
+//        var otel = GlobalOpenTelemetry.get();   // agent SDK
+//        var currentTraceContext = new OtelCurrentTraceContext();
+//        return new OtelTracer(
+//                otel.getTracer("spring-batch"),
+//                currentTraceContext,
+//                event -> {}             // EventPublisher no-op
+//        );
+//    }
+//
+//    @Bean
+//    ObservationRegistry batchObservationRegistry(Tracer tracer) {
+//        var registry = ObservationRegistry.create();
+//        DefaultTracingObservationHandler observationHandler = new DefaultTracingObservationHandler(tracer);
+//        registry.observationConfig()
+//                .observationHandler(observationHandler);
+//        return registry;
+//    }
 
 
     ///// span 필터링 처리방법
