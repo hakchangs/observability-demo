a04fb9d0c6de59cdd4e636f92cf03df3717dce2e
hakchangs <hakchangs@gmail.com>
2026-07-27 15:49:06 +0900

    jdbc query span 추가 테스트

--- service/be-batch/src/main/java/net/kubeworks/bebatch/config/OtelConfig.java
+++ service/be-batch/src/main/java/net/kubeworks/bebatch/config/OtelConfig.java
@@ -1,40 +1,40 @@
 package net.kubeworks.bebatch.config;
 
-//import io.micrometer.observation.Observation;
-//import io.micrometer.observation.ObservationHandler;
-//import io.micrometer.observation.ObservationRegistry;
-//import io.micrometer.tracing.Tracer;
-//import io.micrometer.tracing.handler.DefaultTracingObservationHandler;
-//import io.micrometer.tracing.otel.bridge.OtelCurrentTraceContext;
-//import io.micrometer.tracing.otel.bridge.OtelTracer;
-//import io.opentelemetry.api.GlobalOpenTelemetry;
-//import io.opentelemetry.api.OpenTelemetry;
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationHandler;
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.tracing.Tracer;
+import io.micrometer.tracing.handler.DefaultTracingObservationHandler;
+import io.micrometer.tracing.otel.bridge.OtelCurrentTraceContext;
+import io.micrometer.tracing.otel.bridge.OtelTracer;
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.OpenTelemetry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 public class OtelConfig {
 
     private final Logger log = LoggerFactory.getLogger(OtelConfig.class);
 
-//    @Bean
-//    OpenTelemetry openTelemetry() {
-//        return GlobalOpenTelemetry.get();   // agent SDK 재사용 — 이게 핵심
-//    }
-//
-//    @Bean
-//    Tracer tracer() {
-//        var otel = GlobalOpenTelemetry.get();   // agent SDK
-//        var currentTraceContext = new OtelCurrentTraceContext();
-//        return new OtelTracer(
-//                otel.getTracer("spring-batch"),
-//                currentTraceContext,
-//                event -> {}             // EventPublisher no-op
-//        );
-//    }
-//
+    @Bean
+    OpenTelemetry openTelemetry() {
+        return GlobalOpenTelemetry.get();   // agent SDK 재사용 — 이게 핵심
+    }
+
+    @Bean
+    Tracer tracer() {
+        var otel = GlobalOpenTelemetry.get();   // agent SDK
+        var currentTraceContext = new OtelCurrentTraceContext();
+        return new OtelTracer(
+                otel.getTracer("spring-batch"),
+                currentTraceContext,
+                event -> {}             // EventPublisher no-op
+        );
+    }
+
 //    @Bean
 //    ObservationRegistry batchObservationRegistry(Tracer tracer) {
 //        var registry = ObservationRegistry.create();
@@ -62,26 +62,26 @@ public class OtelConfig {
 //        return registry;
 //    }
 
-//    //방법2. handler 오버라이딩 필터링: span 만 수집제외
-//    @Bean
-//    ObservationRegistry batchObservationRegistry(Tracer tracer) {
-//        var registry = ObservationRegistry.create();
-//        var tracingHandler = new DefaultTracingObservationHandler(tracer);
-//        registry.observationConfig().observationHandler(new ObservationHandler<>() {
-//            @Override public boolean supportsContext(Observation.Context context) {
-//                if (context.getName() == null) return false;
-//                var name = context.getName();
-//                log.info("[OBS] name={}, contextual={}, lowKeys={}",
-//                        context.getName(), context.getContextualName(), context.getLowCardinalityKeyValues());
-//                return (name.equals("spring.batch.job") || name.equals("spring.batch.step"))
-//                        && tracingHandler.supportsContext(context);
-//            }
-//            @Override public void onStart(Observation.Context context) { tracingHandler.onStart(context); }
-//            @Override public void onStop(Observation.Context context) { tracingHandler.onStop(context); }
-//            @Override public void onError(Observation.Context context) { tracingHandler.onError(context); }
-//            @Override public void onScopeOpened(Observation.Context context) { tracingHandler.onScopeOpened(context); }
-//            @Override public void onScopeClosed(Observation.Context context) { tracingHandler.onScopeClosed(context); }
-//        });
-//        return registry;
-//    }
+    //방법2. handler 오버라이딩 필터링: span 만 수집제외
+    @Bean
+    ObservationRegistry batchObservationRegistry(Tracer tracer) {
+        var registry = ObservationRegistry.create();
+        var tracingHandler = new DefaultTracingObservationHandler(tracer);
+        registry.observationConfig().observationHandler(new ObservationHandler<>() {
+            @Override public boolean supportsContext(Observation.Context context) {
+                if (context.getName() == null) return false;
+                var name = context.getName();
+                log.info("[OBS] name={}, contextual={}, lowKeys={}",
+                        context.getName(), context.getContextualName(), context.getLowCardinalityKeyValues());
+                return (name.equals("spring.batch.job") || name.equals("spring.batch.step"))
+                        && tracingHandler.supportsContext(context);
+            }
+            @Override public void onStart(Observation.Context context) { tracingHandler.onStart(context); }
+            @Override public void onStop(Observation.Context context) { tracingHandler.onStop(context); }
+            @Override public void onError(Observation.Context context) { tracingHandler.onError(context); }
+            @Override public void onScopeOpened(Observation.Context context) { tracingHandler.onScopeOpened(context); }
+            @Override public void onScopeClosed(Observation.Context context) { tracingHandler.onScopeClosed(context); }
+        });
+        return registry;
+    }
 }
