fd579db711fde7d8bb3aec459ba2b8b038840301
hakchangs <hakchangs@gmail.com>
2026-07-28 10:09:11 +0900

    be-controller 추가

---
+++ service/be-controller/src/main/java/net/kubeworks/becontroller/BeControllerApplication.java
@@ -0,0 +1,13 @@
+package net.kubeworks.becontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeControllerApplication.class, args);
+    }
+
+}
---
+++ service/be-controller/src/test/java/net/kubeworks/becontroller/BeControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kubeworks.becontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
