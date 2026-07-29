16372ae5ca233856066653f8f29d0331e8420860
hakchangs <hakchangs@gmail.com>
2026-07-29 10:50:04 +0900

    job param 전달 테스트

--- service/be-controller/src/main/java/net/kubeworks/becontroller/service/BatchJobLauncher.java
+++ service/be-controller/src/main/java/net/kubeworks/becontroller/service/BatchJobLauncher.java
@@ -76,6 +76,7 @@ public class BatchJobLauncher {
                         .addNewEnv().withName("TRACE_PARENT").withValue(traceparent == null ? "" : traceparent).endEnv()
                         .addNewEnv().withName("TRACE_STATE").withValue(tracestate).endEnv()
                         .addNewEnv().withName("CORRELATION_GUID").withValue(guid).endEnv()
+                        .withArgs("--guid=" + guid + ",java.lang.String,false")
                     .endContainer().endSpec().endTemplate()
                 .endSpec()
                 .build();
