52d15391c346bd2a890d91e666e707c24ec6dc95
hakchangs <hakchangs@gmail.com>
2026-07-29 11:40:12 +0900

    baggage 방식 배치전파 테스트

--- service/be-batch/src/main/java/net/kubeworks/bebatch/shared/CommonJobExecutionListener.java
+++ service/be-batch/src/main/java/net/kubeworks/bebatch/shared/CommonJobExecutionListener.java
@@ -1,7 +1,11 @@
 package net.kubeworks.bebatch.shared;
 
+import io.opentelemetry.api.baggage.Baggage;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.batch.core.job.JobExecution;
 import org.springframework.batch.core.job.parameters.JobParameters;
 import org.springframework.batch.core.listener.JobExecutionListener;
@@ -13,17 +17,30 @@ public class CommonJobExecutionListener implements JobExecutionListener, Ordered
 
     private final Logger log = LoggerFactory.getLogger(CommonJobExecutionListener.class);
 
+    private static final ThreadLocal<Scope> BAGGAGE_SCOPE = new ThreadLocal<>();
+
     @Override
     public void beforeJob(JobExecution jobExecution) {
         log.info("beforeJob...");
         JobParameters params = jobExecution.getJobParameters();
         log.info("job params = {}", params.parameters());
         log.info("guid param = {}", params.getString("guid"));
+
+        String guid = params.getString("guid");
+        if (guid != null && !guid.isBlank()) {
+            MDC.put("guid", guid);
+            Baggage baggage = Baggage.current().toBuilder().put("guid", guid).build();
+            Scope scope = baggage.storeInContext(Context.current()).makeCurrent();
+            BAGGAGE_SCOPE.set(scope);
+        }
     }
 
     @Override
     public void afterJob(JobExecution jobExecution) {
         log.info("afterJob...");
+        Scope scope = BAGGAGE_SCOPE.get();
+        if (scope != null) { scope.close(); BAGGAGE_SCOPE.remove(); }
+        MDC.remove("guid");
     }
 
     @Override
