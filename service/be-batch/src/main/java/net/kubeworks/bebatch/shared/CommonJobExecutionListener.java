728acea4367b418a11a161090729bd0890986f72
hakchangs <hakchangs@gmail.com>
2026-07-29 10:49:44 +0900

    job param 출력 확인 테스트

--- service/be-batch/src/main/java/net/kubeworks/bebatch/shared/CommonJobExecutionListener.java
+++ service/be-batch/src/main/java/net/kubeworks/bebatch/shared/CommonJobExecutionListener.java
@@ -3,6 +3,7 @@ package net.kubeworks.bebatch.shared;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.job.JobExecution;
+import org.springframework.batch.core.job.parameters.JobParameters;
 import org.springframework.batch.core.listener.JobExecutionListener;
 import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
@@ -15,6 +16,9 @@ public class CommonJobExecutionListener implements JobExecutionListener, Ordered
     @Override
     public void beforeJob(JobExecution jobExecution) {
         log.info("beforeJob...");
+        JobParameters params = jobExecution.getJobParameters();
+        log.info("job params = {}", params.getIdentifyingParameters());
+        log.info("guid param = {}", params.getString("guid"));
     }
 
     @Override
