ec166a892758b374cd46226c3d0276195acbd4a4
hakchangs <hakchangs@gmail.com>
2026-07-30 09:40:56 +0900

    온라인 호출 테스트 코드 추가

--- service/be-product/src/main/java/net/kubeworks/beproduct/service/BatchTriggerService.java
+++ service/be-product/src/main/java/net/kubeworks/beproduct/service/BatchTriggerService.java
@@ -1,7 +1,6 @@
 package net.kubeworks.beproduct.service;
 
 import org.springframework.http.HttpStatusCode;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
@@ -11,14 +10,16 @@ public class BatchTriggerService {
 
     private final RestClient restClient;
 
-    public BatchTriggerService(RestClient restClient) {
-        this.restClient = restClient;
+    public BatchTriggerService(RestClient.Builder restClientBuilder) {
+        this.restClient = restClientBuilder
+                .baseUrl("http://be-controller:8080")
+                .defaultHeader("Content-Type", "application/json")
+                .build();
     }
 
     public boolean triggerLongRunningProbeJob() {
         ResponseEntity<BatchResponse> responseEntity = restClient.post()
-                .uri("http://be-controller:8080/internal/batch/trigger")
-                .contentType(MediaType.APPLICATION_JSON)
+                .uri("/internal/batch/trigger")
                 .body(new BatchRequest("longProbeJob"))
                 .retrieve()
                 .toEntity(BatchResponse.class);
