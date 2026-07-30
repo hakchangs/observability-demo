3d71afd54a1f485830c0272a2622409a8ef1b2de
hakchangs <hakchangs@gmail.com>
2026-07-30 10:56:32 +0900

    baggage 환경변수 추가 전달

--- service/be-batch/src/main/java/net/kubeworks/bebatch/config/BatchConfig.java
+++ service/be-batch/src/main/java/net/kubeworks/bebatch/config/BatchConfig.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class BatchConfig {
 
     @Bean
